b2e2bf199ff10ba4235e404f074143870a2e1544
PaweliOS <mirrorpawel.pabisiak@gmail.com>
2022-11-16 13:35:56 +0100

    Added === rplant === (script.js) and neccessary varies: const net .., pool ..., miner ... . const URL_NET..., URL_POOL..., URL_MINER, poolHashrateValue, minerHashrateValue, blocksday , functions: calcCoinsDay and round(X_Y). Adjust class for <p> in index.html (below row rplant).

--- js/script.js
+++ js/script.js
@@ -4,6 +4,20 @@ const ethPrice = document.querySelector('.crypto__coins--eth-price')
 const qogePrice = document.querySelector('.crypto__coins--qoge-price')
 const plsrPrice = document.querySelector('.crypto__coins--plsr-price')
 
+// ============ rplant ============//
+const netHashrate = document.querySelector('.crypto__net-hashrate')
+
+const poolWorkers = document.querySelector('.crypto__pool-workers')
+const poolHashrate = document.querySelector('.crypto__pool-hashrate')
+
+const minerWorkers = document.querySelector('.crypto__miner-workers')
+const minerHashrate = document.querySelector('.crypto__miner-hashrate')
+const minerShares = document.querySelector('.crypto__miner-shares')
+const minerCoinsDay = document.querySelector('.crypto__miner-coins-day')
+
+
+
+
 // const URL = 'https://api.coingecko.com/api/v3/exchange_rates'
 
 // === poniżej tylko dla wybranej crypto sama cena ====
@@ -17,6 +31,16 @@ const URL_ETH_USD = 'https://api.coingecko.com/api/v3/simple/price?ids=' + ETH +
 
 const URL_EXBITRON_BTC = 'https://www.exbitron.com/api/v2/peatio/public/markets/'+EXBITRON_qogeusdt+'/order-book'
 const URL_EXBITRON_PLSR = 'https://www.exbitron.com/api/v2/peatio/public/markets/'+EXBITRON_plsrusdt+'/order-book'
+
+// ========= rplant ========= //
+const URL_NET_HASHRATE = 'https://pool.rplant.xyz/api/stats'
+const URL_POOL_WORKERS = 'https://pool.rplant.xyz/api/stats'
+const URL_POOL_HASHRATE = 'https://pool.rplant.xyz/api/stats'
+const URL_MINER = 'https://pool.rplant.xyz/api/walletEx/qogecoin/bq1q3x2jxmvz7w0h74s7zys4jsnt7u80066fp3mqh4'
+
+let poolHashrateValue
+let minerHashrateValue
+const blocksday = 858+190
 
 // === poniżej dla jednego coina (ethereum) pełna informacja =====
 // const URL = 'https://api.coingecko.com/api/v3/coins/ethereum'
@@ -50,7 +74,13 @@ async function getPrice() {
     const respEth = await axios.get(URL_ETH_USD)
     const respExbitronQoge = await axios.get(URL_EXBITRON_BTC)
     const respExbitronPlsr = await axios.get(URL_EXBITRON_PLSR)
-    
+    const respRplantNetHashrate = await axios.get(URL_NET_HASHRATE)
+    const respRplantPoolWorkers = await axios.get(URL_POOL_WORKERS)
+    const respRplantPoolHashrate = await axios.get(URL_POOL_HASHRATE)
+
+    const respRplantPoolMiner = await axios.get(URL_MINER)
+    // const respRplantPoolMiner = await axios.get(URL_MINER)
+
     // console.log(respExbitronQoge.data.bids[0].price)
     // console.log(respExbitronQoge.data.bids[0].price)
     
@@ -58,8 +88,32 @@ async function getPrice() {
     plsrPrice.textContent = respExbitronPlsr.data.bids[0].price + ' USD'
     btcPrice.textContent = respBtc.data.bitcoin.usd + ' USD'
     ethPrice.textContent = respEth.data.ethereum.usd + ' USD'
+    netHashrate.textContent = respRplantNetHashrate.data.pools.qogecoin.poolStats.networkSols + ' kH/s'
+    poolWorkers.textContent = respRplantPoolWorkers.data.pools.qogecoin.poolStats.workerCount 
+    poolHashrateValue = respRplantPoolHashrate.data.pools.qogecoin.hashrate
+    poolHashrate.textContent = poolHashrateValue
+    minerWorkers.textContent = respRplantPoolMiner.data.miners.length
+    minerHashrateValue = respRplantPoolMiner.data.hashrate
+    minerHashrate.textContent = minerHashrateValue
+    minerShares.textContent = respRplantPoolMiner.data.shares
+    minerCoinsDay.textContent = roundX_Y(calcCoinsDay(poolHashrateValue, minerHashrateValue), 1)
+    console.log(respRplantPoolMiner.data.miners.length)
+
+}
+
+
+const calcCoinsDay = (poolHashrateValue, minerHashrateValue) => {
+    let output = minerHashrateValue * blocksday / poolHashrateValue * 50
+    return output
     
 }
+
+// roundX_Y zaokrąglij liczbę X do Y miejsc po przecinku
+const roundX_Y = (x,y) => {
+    let output = (Math.round(x * Math.pow(10, y)))/Math.pow(10, y)
+    return output
+}
+
 
 // for (n = 0; n = 3600; n++){
 //     sleep(1000).console.log('ciach')
